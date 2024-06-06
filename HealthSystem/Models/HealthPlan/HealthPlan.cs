5bf8a278646f550afb4890b9f414d62ae0c3da20
a-vortex <mccontarinid@gmail.com>
2024-06-06 10:41:55 -0300

    correcting some issues with healthplan class

--- HealthSystem/Models/HealthPlan/HealthPlan.cs
+++ HealthSystem/Models/HealthPlan/HealthPlan.cs
@@ -1,14 +1,14 @@
-﻿abstract class HealthPlan 
+﻿public class HealthPlan 
 {
     protected int PlanId { get; private set; }
     protected string Name { get; private set; }
     protected float Price { get; private set; }
+    protected string Description { get; private set; }
 
-    protected HealthPlan(string name, float price)
+    protected HealthPlan(string name, float price, string description)
     {
         this.Name = name;
         this.Price = price; 
+        this.Description = description;
     }
-
-    public abstract string Description();
 }
