35ffb538fc6e9216595077c906c3c8a7338957dd
a-vortex <mccontarinid@gmail.com>
2024-06-06 23:04:54 -0300

    Create Doctor sub-class

---
+++ HealthSystem/Models/User/Doctor.cs
@@ -0,0 +1,4 @@
+class Doctor(string login, string password) : User(login, password)
+{
+    
+}
--- HealthSystem/Program.cs
+++ HealthSystem/Program.cs
@@ -1,8 +1,1 @@
-﻿class Doctor : User
-{
-    public Doctor(string login, string password) : base(login, password)
-    {
-    }
-
-}
-
+﻿
