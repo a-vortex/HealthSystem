23724324ae8505f1aaf669453c5ed3abaf733fa4
a-vortex <mccontarinid@gmail.com>
2024-06-10 13:53:04 -0300

    modificações gerais

--- HealthSystem/Models/MedicalServices/MedicalServices.cs
+++ HealthSystem/Models/MedicalServices/MedicalServices.cs
@@ -1,8 +1,7 @@
-public class MedicalServices
+public class MedicalServices(string name, float price, string description)
 {
-        // TODO: Add properties, methods, and fields here
-
-        // TODO: Implement constructors here
-
-        // TODO: Implement additional methods here
+    public int MedicalServiceId { get; private set; }
+    public string Name { get; private set; } = name;
+    public float Price { get; private set; } = price;
+	public string Description { get; private set; } = description;
 }
