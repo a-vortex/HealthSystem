e0b66ed24f0f7879526190390b2b087c10a8a34d
a-vortex <mccontarinid@gmail.com>
2024-06-10 09:47:26 -0300

    create HealthSystemDbContext

---
+++ HealthSystem/Data/HealthSystemDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ConsoleAppDb;Trusted_Connection=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasDiscriminator<string>("UserType")
+            .HasValue<Admin>("Admin")
+            .HasValue<Customer>("Customer");
+
+        modelBuilder.Entity<User>()
+            .OwnsOne(u => u.PersonalInfo);
+    }
+}
