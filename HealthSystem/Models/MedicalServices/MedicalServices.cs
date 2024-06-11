064d07a745bf25b0be03020bc4323d46da5be85a
a-vortex <mccontarinid@gmail.com>
2024-06-11 11:46:01 -0300

    Added MedicalService Area and Type

--- HealthSystem/Models/MedicalServices/MedicalServices.cs
+++ HealthSystem/Models/MedicalServices/MedicalServices.cs
@@ -4,12 +4,17 @@ public class MedicalService
     public string? Name { get; private set; }
     public float Price { get; private set; }
     public string? Description { get; private set; }
+    public MedicalServiceType Type { get; private set; }
+    public MedicalServiceArea Area { get; private set; }
 
     public MedicalService() { }
-    public MedicalService(string name, float price, string description)
+    public MedicalService(string name, float price, string description, 
+    MedicalServiceType medicalServiceType, MedicalServiceArea medicalServiceArea)
     {
         Name = name;
         Price = price;
         Description = description;
+        Type = medicalServiceType;
+        Area = medicalServiceArea;
     }
 }
