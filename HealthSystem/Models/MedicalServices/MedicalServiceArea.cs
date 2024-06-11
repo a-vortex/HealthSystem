907a4eca0d9b8edd53244d74f08514648c7e36e4
a-vortex <mccontarinid@gmail.com>
2024-06-11 11:43:42 -0300

    Create MedicalService Area and Type enums

---
+++ HealthSystem/Models/MedicalServices/MedicalServiceArea.cs
@@ -0,0 +1,25 @@
+public enum MedicalServiceArea
+{
+    Acupuncture,
+    Allergy,
+    Anesthesiology,
+    Cardiology,
+    Dermatology,
+    Endocrinology,
+    Gastroenterology,
+    GeneralPractice,
+    Hematology,
+    InfectiousDisease,
+    InternalMedicine,
+    Nephrology,
+    Neurology,
+    ObstetricsAndGynecology,
+    Oncology,
+    Ophthalmology,
+    Orthopedics,
+    Otolaryngology,
+    Pediatrics,
+    PhysicalMedicineAndRehabilitation,
+    Psychiatry,
+    Urology
+}
---
+++ HealthSystem/Models/MedicalServices/MedicalServiceType.cs
@@ -0,0 +1,6 @@
+public enum MedicalServiceType
+{
+    Consultation,
+    Exam,
+    Surgery
+}
