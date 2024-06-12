fe1b5d7d237542d9a3fea864544bc9bc708a4dd3
a-vortex <mccontarinid@gmail.com>
2024-06-11 22:06:27 -0300

    adiciona a chave estrangeira de healthplan em customer

--- HealthSystem/Models/User/Customer.cs
+++ HealthSystem/Models/User/Customer.cs
@@ -3,9 +3,14 @@
 public class Customer : User
 {
     public HealthPlan HealthPlan {get;private set;} = new HealthPlan();
+    public int HealthPlanId { get; private set; } //Foreign Key
 
-    public Customer() : base() { }
+    public Customer() : base() => HealthPlanId = HealthPlan.PlanId;
     public Customer(string login, string password, Personal personalInfo, string name, string address, string email, int telephone) :
-    base(login, password, personalInfo, name, address, email, telephone) { }
-    public void SetHealthPlan(HealthPlan healthPlan) => HealthPlan = healthPlan;
+    base(login, password, personalInfo, name, address, email, telephone) => HealthPlanId = HealthPlan.PlanId;
+    public void SetHealthPlan(HealthPlan healthPlan) 
+    {
+        HealthPlan = healthPlan;
+        HealthPlanId = healthPlan.PlanId;
+    }
 }
