dab1d814da88c7e1188d9d9751afbc477b5a1d4d
a-vortex <mccontarinid@gmail.com>
2024-06-13 16:28:00 -0300

    Big arquitecture update :)

--- HealthSystem/Models/HealthPlans/HealthPlan.cs
+++ HealthSystem/Models/HealthPlans/HealthPlan.cs
@@ -1,10 +1,12 @@
-﻿public class HealthPlan
+﻿using System.Collections.Generic;
+
+public class HealthPlan
 {
     public int PlanId { get; private set; } //Primary Key
     public string? Name { get; private set; } 
     public float? Price { get; private set; }
     public string? Description { get; private set; }
-    public List<MedicalServiceType> Coverages { get; private set; } = [];
+    public List<MedicalServiceType> Coverages { get; private set; } = new List<MedicalServiceType>();
 
     public HealthPlan() {}
     public HealthPlan(string name, float price, string description)
